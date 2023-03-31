9dc96206e6e8a9bcf86315ea6f00c39d342c54fc
Michael Haar <michael.haar92@gmail.com>
2023-03-31 15:48:35 +0200

    Update app.ts

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world',
+                message: 'hello michi',
             }),
         };
     } catch (err) {
