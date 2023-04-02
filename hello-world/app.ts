3e0cf2c3084bd6f7f4eb43a695002e833c7e936e
Michael Haar <michael.haar92@gmail.com>
2023-04-02 11:37:53 +0200

    add min function runtime

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -11,20 +11,35 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  */
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
+    return useMinFunctionRuntime(async () => {
+        try {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'hello world',
+                }),
+            };
+        } catch (err) {
+            console.log(err);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: 'some error happened',
+                }),
+            };
+        }
+    });
+};
+
+const useMinFunctionRuntime = async <T>(func: () => Promise<T>, minRuntimeInMs = 1000): Promise<T> => {
+    const start = Date.now();
+    const result = func();
+    const end = Date.now();
+
+    if (end - start < minRuntimeInMs) {
+        const sleepTime = minRuntimeInMs - (end - start);
+        await new Promise((resolve) => setTimeout(resolve, sleepTime));
     }
+
+    return result;
 };
--- hello-world/tests/unit/test-handler.test.ts
+++ hello-world/tests/unit/test-handler.test.ts
@@ -2,57 +2,16 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { lambdaHandler } from '../../app';
 
 describe('Unit test for app handler', function () {
+    it('runs at least 1 second', async () => {
+        const start = Date.now();
+        await lambdaHandler(testEvent);
+        const end = Date.now();
+
+        expect(end - start).toBeGreaterThanOrEqual(1000);
+    });
+
     it('verifies successful response', async () => {
-        const event: APIGatewayProxyEvent = {
-            httpMethod: 'get',
-            body: '',
-            headers: {},
-            isBase64Encoded: false,
-            multiValueHeaders: {},
-            multiValueQueryStringParameters: {},
-            path: '/hello',
-            pathParameters: {},
-            queryStringParameters: {},
-            requestContext: {
-                accountId: '123456789012',
-                apiId: '1234',
-                authorizer: {},
-                httpMethod: 'get',
-                identity: {
-                    accessKey: '',
-                    accountId: '',
-                    apiKey: '',
-                    apiKeyId: '',
-                    caller: '',
-                    clientCert: {
-                        clientCertPem: '',
-                        issuerDN: '',
-                        serialNumber: '',
-                        subjectDN: '',
-                        validity: { notAfter: '', notBefore: '' },
-                    },
-                    cognitoAuthenticationProvider: '',
-                    cognitoAuthenticationType: '',
-                    cognitoIdentityId: '',
-                    cognitoIdentityPoolId: '',
-                    principalOrgId: '',
-                    sourceIp: '',
-                    user: '',
-                    userAgent: '',
-                    userArn: '',
-                },
-                path: '/hello',
-                protocol: 'HTTP/1.1',
-                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
-                requestTimeEpoch: 1428582896000,
-                resourceId: '123456',
-                resourcePath: '/hello',
-                stage: 'dev',
-            },
-            resource: '',
-            stageVariables: {},
-        };
-        const result: APIGatewayProxyResult = await lambdaHandler(event);
+        const result: APIGatewayProxyResult = await lambdaHandler(testEvent);
 
         expect(result.statusCode).toEqual(200);
         expect(result.body).toEqual(
@@ -62,3 +21,53 @@ describe('Unit test for app handler', function () {
         );
     });
 });
+
+const testEvent: APIGatewayProxyEvent = {
+    httpMethod: 'get',
+    body: '',
+    headers: {},
+    isBase64Encoded: false,
+    multiValueHeaders: {},
+    multiValueQueryStringParameters: {},
+    path: '/hello',
+    pathParameters: {},
+    queryStringParameters: {},
+    requestContext: {
+        accountId: '123456789012',
+        apiId: '1234',
+        authorizer: {},
+        httpMethod: 'get',
+        identity: {
+            accessKey: '',
+            accountId: '',
+            apiKey: '',
+            apiKeyId: '',
+            caller: '',
+            clientCert: {
+                clientCertPem: '',
+                issuerDN: '',
+                serialNumber: '',
+                subjectDN: '',
+                validity: { notAfter: '', notBefore: '' },
+            },
+            cognitoAuthenticationProvider: '',
+            cognitoAuthenticationType: '',
+            cognitoIdentityId: '',
+            cognitoIdentityPoolId: '',
+            principalOrgId: '',
+            sourceIp: '',
+            user: '',
+            userAgent: '',
+            userArn: '',
+        },
+        path: '/hello',
+        protocol: 'HTTP/1.1',
+        requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
+        requestTimeEpoch: 1428582896000,
+        resourceId: '123456',
+        resourcePath: '/hello',
+        stage: 'dev',
+    },
+    resource: '',
+    stageVariables: {},
+};
